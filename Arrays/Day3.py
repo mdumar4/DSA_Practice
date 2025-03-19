fb49834bc4cbf040128ef0bb5fe0e71ddd17d4c3
mdumar4 <mdumr4@gmail.com>
2025-03-20 00:06:20 +0530

    Day 3 Complete

---
+++ Arrays/Day3.py
@@ -0,0 +1,13 @@
+# Reverse an Array
+
+def reversearray(arr):
+    end = len(arr)-1
+    for i in range(len(arr)//2):
+        arr[i], arr[end] = arr[end], arr[i]
+        end -=1
+    return arr
+
+
+if __name__ == "__main__":
+    arr = [1, 2, 3, 4, 5, 6]
+    print(reversearray(arr))
